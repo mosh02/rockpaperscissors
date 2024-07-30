5f1bc2625ca50cb2cc8f3df1f9040cecebd87698
mosh <158522662+mosh02@users.noreply.github.com>
2024-07-29 21:27:48 -0400

    finished the code

--- script.js
+++ script.js
@@ -2,22 +2,97 @@ console.log("Hello World")
 
 function getComputerChoice() {
     let x = Math.random()
-
     if (x <= 0.33) {
-        return "Rock"
+        choice = "rock"
+        console.log("Computer: " + choice)
+        return choice
     }
 
     else if (0.67<=x>0.33) {
-        return "Paper"
+        choice = "paper"
+        console.log("Computer: " + choice)
+        return choice
     }
 
     else {
-        return "Scissors"
+        choice = "scissors"
+        console.log("Computer: " + choice)
+        return choice
     }
+
 }
 
 function getHumanChoice() {
+    check = 0;
     let choice = prompt("Please enter rock paper or scissors: ")
+    choice = choice.toLowerCase()
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        check = 1
+        console.log("Human: " + choice)
+        return choice
+
+    }
+    else {
+        check = 0
+        console.log("invalid choice")
+    }
 }
 
-console.log(getComputerChoice())
+
+
+let humanScore = 0
+let computerScore = 0
+
+
+
+function playRound(humanChoice, computerChoice) { 
+        
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore++;
+            console.log("human has won")
+        }
+        
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++;
+            console.log("human has won")
+        }
+
+        else if (humanChoice == "paper" && computerChoice == "rock") { 
+            humanScore++;
+            console.log("human has won")
+        }
+
+        else if (humanChoice === computerChoice) { 
+            console.log("it is a tie")           
+        }
+
+        else if (check === 0) {
+            console.log("invalid choice by human")
+        }
+
+        else {
+            computerScore++
+            console.log("computer has won")
+        }
+}
+
+
+
+for (let i = 0; i<5; i++) {
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log("Human: " + humanScore)
+    console.log("Computer: " + computerScore)
+}
+
+console.log("GAME OVER")
+
+if (humanScore > computerScore) {
+    console.log("Congratulations Human, you are the victor")
+}
+else if (humanScore < computerScore) { 
+    console.log("Unlucky! Try again")
+}
+
+else { 
+    console.log("The game ends in a tie")
+}
